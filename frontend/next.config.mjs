a643af6593f61e762ed414a1065b85da460b6f2e
Eliza <eliza.kryshtal@gmail.com>
2026-09-09 17:02:47 +0200

    fix: refactored front structure

--- frontend/next.config.mjs
+++
@@ -1,7 +0,0 @@
-const backend = process.env.BACKEND_URL || "http://backend:5000";
-
-export default {
-  async rewrites() {
-    return [{ source: "/api/highscores", destination: `${backend}/` }];
-  },
-};
